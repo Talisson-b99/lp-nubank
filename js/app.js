812c06e865d8df88a7353f2c1ff158590f8ccc32
talisson <talissonb422@gmail.com>
2024-03-11 04:44:53 -0300

    add motion scroll

--- js/app.js
+++ js/app.js
@@ -3,8 +3,7 @@ function scrollHeader(){
   const nav = document.getElementById('header')
   
   if(this.scrollY >= 50) nav.classList.add('active-header'); else nav.classList.remove('active-header')
-  console.log('ativo')
-console.log()
+
 }
 
 window.addEventListener('scroll', scrollHeader)
